94e903f26ba3bc0da36dfa49697b529ad1dac861
Piotrek5994 <piotrek5994@gmail.com>
2022-11-17 21:27:23 +0100

    Initial commit

---
+++ BankTransaction/Models/TramsactionDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankTransaction.Models
+{
+    public class TramsactionDbContext : DbContext
+    {
+        public TramsactionDbContext(DbContextOptions<TramsactionDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+    
+    
+}
