4daf5910669c0a7b57ea1af75d7429cab6951902
Piotrek5994 <piotrek5994@gmail.com>
2022-11-22 20:45:54 +0100

    Conecction with Admin Panel

---
+++ BankTransaction/Models/AdminModelContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankTransaction.Models;
+
+public class AdminModelContext : IdentityDbContext<User>
+{
+    public AdminModelContext(DbContextOptions<TransactionDbContext> options) : base(options)
+    {
+
+    }
+    public DbSet<Transaction> Transaction { get; set; }
+
+    public DbSet<User> User { get; set; }
+
+} 
--- BankTransaction/Models/TransactionDbContext.cs
+++ BankTransaction/Models/TransactionDbContext.cs
@@ -8,7 +8,7 @@ public class TransactionDbContext : IdentityDbContext<User>
 {
     public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
     {
-        
+           
     }
 
     public DbSet<Transaction> Transactions { get; set; }
